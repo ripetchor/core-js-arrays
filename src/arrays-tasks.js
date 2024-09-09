b8e48310ac2b84656cffd01255cb6f84f9ffa53e
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2024-09-09 22:34:13 +0500

    feat: add solution for getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -122,8 +122,13 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+
+function getAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  const avg = arr.reduce((acc, curr) => acc + curr ?? 0, 0) / arr.length;
+  return Number.isInteger(avg) ? avg : Number(avg.toFixed(2));
 }
 
 /**
